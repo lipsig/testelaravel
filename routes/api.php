a59990235624da08dff3924dc041ad351a09b26c
Felipe Lourenzi <felipesiegle@gmail.com>
2024-03-21 19:05:38 -0300

    update: Student Factory, test

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\LoanController;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\StudentController;
 
 Route::middleware('auth:api')->group(function () {
     Route::apiResource('authors', AuthorController::class);
     Route::apiResource('books', BookController::class);
     Route::apiResource('loans', LoanController::class)->except(['update', 'destroy']);
+    Route::apiResource('students', StudentController::class); 
 });
 
 
