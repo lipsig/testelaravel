3efb0fefdfc091604695fa40d738d0bbba582024
Felipe Lourenzi <felipesiegle@gmail.com>
2024-03-21 18:55:58 -0300

    update: loan crud

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -40,4 +40,37 @@ class LoanController extends Controller
     {
         return $loan->load(['student', 'book']);
     }
+
+    public function update(Request $request, Loan $loan)
+    {
+        $messages = [
+            'student_id.required' => 'The student ID field is required.',
+            'student_id.exists' => 'The selected student ID is invalid.',
+            'book_id.required' => 'The book ID field is required.',
+            'book_id.exists' => 'The selected book ID is invalid.',
+            'loan_date.required' => 'The loan date field is required.',
+            'loan_date.date' => 'The loan date is not a valid date.',
+            'return_date.required' => 'The return date field is required.',
+            'return_date.date' => 'The return date is not a valid date.',
+            'return_date.after_or_equal' => 'The return date must be a date after or equal to the loan date.',
+        ];
+
+        $request->validate([
+            'student_id' => 'required|exists:students,id',
+            'book_id' => 'required|exists:books,id',
+            'loan_date' => 'required|date',
+            'return_date' => 'required|date|after_or_equal:loan_date',
+        ], $messages);
+
+        $loan->update($request->all());
+
+        return $loan;
+    }
+
+    public function destroy(Loan $loan)
+    {
+        $loan->delete();
+
+        return response()->json(null, 204);
+    }
 }
