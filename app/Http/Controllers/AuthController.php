b82a957b933cbabbe1320069570174ae3abe50fa
Felipe Lourenzi <felipesiegle@gmail.com>
2024-03-21 17:57:56 -0300

    refactor: auth code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail; 
+use App\Models\User;
 use App\Mail\RegistrationSuccessEmail; 
+
 
 class AuthController extends Controller
 {
