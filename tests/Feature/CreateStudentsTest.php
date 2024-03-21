06fe5758c92817fc06be36ef5531b318396c3972
Felipe Lourenzi <felipesiegle@gmail.com>
2024-03-21 19:08:23 -0300

    update: create student test

---
+++ tests/Feature/CreateStudentsTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Tests\Feature;
+use App\Models\Student;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CreateStudentsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_register()
+    {
+        $studentData = Student::factory()->make()->toArray();
+
+        $response = $this->post('/students', $studentData);
+
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('students', $studentData);
+    }
+}
